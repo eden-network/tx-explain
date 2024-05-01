694b052d13ca4a7d218f5bfe6cd80af9fc678054
ShootInPutIn007 <pajakerkez@gmail.com>
2024-05-01 14:26:50 +0200

    Changing the backend to query address labels and ens datatables instead of relying on the flipside api

--- label.py
+++ label.py
@@ -1,8 +1,6 @@
 import json
 import re
-from flipside import Flipside
 import pandas as pd
-
 
 # Recursively iterate over the json object looking for specified pattern
 def explore_json(obj, items, pattern):
@@ -40,64 +38,60 @@ def format (addresses):
     return addresses_str
 
 
-# Query Flipside
-def query_flipside (addresses_str, endpoint):
-    sql = f"""
-        select address,
-            address_name,
-            label,
-            label_type,
-            label_subtype
-        from ethereum.core.dim_labels 
-        where lower(address) in ({addresses_str})
-        """
-
-    query_result_set = endpoint.query(sql)
-    df = pd.DataFrame(query_result_set)
-    return df
-
-# Put results into a json object
-def to_json(df):
+# Query labels datatables
+def query_labels (addresses_str, labels_dataset, ens_dataset, client):
     try:
-        json_data = []
-        data = df[1][4]
-        if data is None:
-            print("Data is None, returning an empty JSON object")
-            return json.dumps({'address_labels': []}, indent=4)
+        sql = f"""
+            with labels_data as (
+                select address,
+                       address_name
+                from {labels_dataset}
+                where address in ({addresses_str})
+                ),
+            ens_data as (
+                select address,
+                       tag as address_name
+                from {ens_dataset}
+                where address in ({addresses_str})
+                  and address not in (select address from labels_data)
+                ),
+            merged_data as (
+                select * from labels_data
+                union all 
+                select * from ens_data 
+            )
 
-        for index, item in enumerate(data):
-            json_data.append({
-                'address': item[0],
-                'address_name': item[1],
-                'label': item[2],
-                'label_type': item[3],
-                'label_subtype': item[4]
-            })
-    
-        json_object = json.dumps({'address_labels': json_data}, indent=4)
-        return json_object
+            select * from merged_data
+        """
+        df = client.query(sql).result().to_dataframe()
+        return df
     except Exception as e:
-        print("Error at to_json: ", e)
+        print("Error at query_labels: ", e)
+        return pd.DataFrame()
 
 # Fetch address labels
-def fetch_address_labels(sim_data, endpoint):
+def fetch_address_labels(sim_data, labels_dataset, ens_dataset, client):
     address_regex = r'^0x[0-9a-fA-F]{40}$'
     try:
         addresses = extract(sim_data, address_regex)
         addresses_str = format(addresses)
-        labels_data = query_flipside(addresses_str,endpoint)
-        labels_json = to_json(labels_data)
+
+        labels = query_labels (addresses_str, labels_dataset, ens_dataset, client)
+
+        labels_json = labels.to_json(orient='records')
+        labels_json = '{"address_labels": ' + labels_json + '}'
+
         return labels_json
     except Exception as e:
         print("Error at fetch_address_labels: ", e)
 
 # Add address labels to the original sim_data
-def add_labels(sim_data, endpoint):
+def add_labels(sim_data, labels_dataset, ens_dataset, client):
     try:
-        labels_json = json.loads(fetch_address_labels(sim_data, endpoint))
+        labels_json = json.loads(fetch_address_labels(sim_data, labels_dataset, ens_dataset, client))
         for key, value in labels_json.items():
             sim_data[key] = value
-        
+
         return sim_data
     except Exception as e:
         print("Error at add_labels: ", e)
