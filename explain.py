2f1a38eec4a7edd5279206767742767a80970c3f
ShootInPutIn007 <100360636+shootinputin007@users.noreply.github.com>
2024-06-06 10:00:37 +0200

    Update explain.py

--- explain.py
+++ explain.py
@@ -115,11 +115,28 @@ async def main(network, delay_time, max_concurrent_connections, skip_function_ca
     
     api_key = os.getenv('ANTHROPIC_API_KEY')
     anthropic_client = AsyncAnthropic(api_key=api_key)
+    api_key_groq = os.getenv('GROQ_API_KEY')
+    groq_client = AsyncGroq(api_key=api_key_groq)
     semaphore = asyncio.Semaphore(max_concurrent_connections)
     
     tasks = []
+    models={
+        "llama3-70b-8192":"groq",
+        "llama3-8b-8192":"groq",
+        "mixtral-8x7b-32768":"groq",
+        "gemma-7b-it":"groq",
+        "claude-3-haiku-20240307":"anthropic",
+        "claude-3-opus-20240229":"anthropic",
+        "claude-3-sonnet-20240229":"anthropic",
+    }
     for file_path, data in json_data:
-        task = asyncio.create_task(process_json_file(anthropic_client, file_path, data, network, semaphore, delay_time, system_prompt, model))
+        if models[model]=="groq":
+            task = asyncio.create_task(process_json_file(groq_client, file_path, data, network, semaphore, delay_time, system_prompt, model))
+        elif models[model]=="anthropic":
+            task = asyncio.create_task(process_json_file(anthropic_client, file_path, data, network, semaphore, delay_time, system_prompt, model))
+        else:
+            #Assume there is new model that was not added to models list, use Anthropic client by default
+            task = asyncio.create_task(process_json_file(anthropic_client, file_path, data, network, semaphore, delay_time, system_prompt, model))
         tasks.append(task)
     
     await asyncio.gather(*tasks)
